50204b09516ae064d0ecdd6967d244e7fbffaf49
Krishnateja6154 <143614127+Krishnateja6154@users.noreply.github.com>
2023-09-01 20:47:27 +0530

    Add files via upload

---
+++ OOPsAssignments/src/com/shristi/inheritdemos/overloading/Employee.java
@@ -0,0 +1,24 @@
+package com.shristi.overloading;
+
+public class Employee {
+	String name;
+	String designation;
+	
+	double calcBonus(double basicAllowance)
+	{
+		return basicAllowance;
+	}
+	double calcBonus(double basicAllowance,double carAllowance)
+	{
+		return (basicAllowance+carAllowance);
+	}
+	double calcBonus(double basicAllowance,double carAllowance,double houseAllowance)
+	{
+		return (basicAllowance+carAllowance+houseAllowance);
+	}
+	public Employee(String name, String designation) {
+		super();
+		this.name = name;
+		this.designation = designation;
+	}
+}
